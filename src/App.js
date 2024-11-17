d8d22472b4f134d6d9bedf5bf9c7bd32e99ccf42
AnatolyLee <toliklee007@gmail.com>
2024-11-17 23:47:59 +0600

    Commit 2

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const StyledComponent = () => {
+  const divStyle = {
+    backgroundColor: 'yellow', // Светло-голубой фон
+    padding: '20px',         // Внутренние отступы
+    borderRadius: '10px',    // Скругленные углы
+    boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)', // Тень
+    textAlign: 'center',     // Текст по центру
+    color: '#333',           // Темный текст
+    fontSize: '18px',        // Размер шрифта
+    margin: '10px auto',     // Отступы снаружи
+    width: '300px',          // Ширина
+  };
+
+  return (
+    <div style={divStyle}>
+      Коммит 2.
+    </div>
+  );
+};
+
+export default StyledComponent;
